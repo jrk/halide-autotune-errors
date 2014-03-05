7f403a6933bcb608bf3f6246cacb5c19f9b7d574
Jonathan Ragan-Kelley <jrk@csail.mit.edu>
2014-03-05 15:02:14 -0500

    Simplify interpolate-simplest-sched1
    Now down to near-minimal form.

--- interpolate-simplest-sched1.cpp
+++ interpolate-simplest-sched1.cpp
@@ -80,79 +80,36 @@ double now() {
 int main(int argc, char **argv) {
     ImageParam input(Float(32), 3, "input");
 
-    const unsigned int levels = 2;
-
-    Func downsampled[levels];
-    Func downx[levels];
-    Func interpolated[levels];
-    Func upsampled[levels];
-    Func upsampledx[levels];
+    Func upsampled;
+    Func upsampledx;
     Var x("x"), y("y"), c("c");
-
-    downsampled[0] = Func("downsampled");
-    downx[0] = Func("downx");
-    interpolated[0] = Func("interpolated");
-    upsampled[0] = Func("upsampled");
-    upsampledx[0] = Func("upsampledx");
 
     Func clamped("clamped");
     clamped(x, y, c) = input(clamp(x, 0, input.width()-1), clamp(y, 0, input.height()-1), c);
 
-    // This triggers a bug in llvm 3.3 (3.2 and trunk are fine), so we
-    // rewrite it in a way that doesn't trigger the bug. The rewritten
-    // form assumes the input alpha is zero or one.
-    // downsampled[0](x, y, c) = select(c < 3, clamped(x, y, c) * clamped(x, y, 3), clamped(x, y, 3));
-    #if 0
-    // downsampled[0](x, y, c) = clamped(x, y, c) * clamped(x, y, 3);
-    downsampled[0] = clamped;
+    upsampledx = Func("upsampledx");
+    upsampled = Func("upsampled");
+    upsampledx(x, y, c) = select((x % 2) == 0,
+                                    clamped(x/2, y, c),
+                                    0.5f * (clamped(x/2, y, c) +
+                                            clamped(x/2+1, y, c)));
+    upsampled(x, y, c) = select((y % 2) == 0,
+                                   upsampledx(x, y/2, c),
+                                   0.5f * (upsampledx(x, y/2, c) +
+                                           upsampledx(x, y/2+1, c)));
 
-    // for (unsigned int l = 1; l < levels; ++l) {
-        downx[1] = Func("downx");
-        downsampled[1] = Func("downsampled");
-        downx[1](x, y, c) = (downsampled[0](x*2-1, y, c) +
-                             2.0f * downsampled[0](x*2, y, c) +
-                             downsampled[0](x*2+1, y, c)) * 0.25f;
-        downsampled[1](x, y, c) = (downx[1](x, y*2-1, c) +
-                                   2.0f * downx[1](x, y*2, c) +
-                                   downx[1](x, y*2+1, c)) * 0.25f;
-    // }
-    interpolated[1] = Func("interpolated");
-    interpolated[1](x, y, c) = downsampled[1](x, y, c);
-    #else
-    interpolated[1] = clamped;
-    #endif
-    // for (unsigned int l = levels-2; l < levels; --l) {
-        upsampledx[0] = Func("upsampledx");
-        upsampled[0] = Func("upsampled");
-        interpolated[0] = Func("interpolated");
-        upsampledx[0](x, y, c) = select((x % 2) == 0,
-                                        interpolated[1](x/2, y, c),
-                                        0.5f * (interpolated[0+1](x/2, y, c) +
-                                                interpolated[0+1](x/2+1, y, c)));
-        upsampled[0](x, y, c) = select((y % 2) == 0,
-                                       upsampledx[0](x, y/2, c),
-                                       0.5f * (upsampledx[0](x, y/2, c) +
-                                               upsampledx[0](x, y/2+1, c)));
-        // interpolated[0](x, y, c) = downsampled[0](x, y, c) + (1.0f - downsampled[0](x, y, 3)) * upsampled[0](x, y, c);
-    // }
-
-    Func final = upsampled[0];
+    Func final = upsampled;
     {
         std::map<std::string, Halide::Internal::Function> funcs = Halide::Internal::find_transitive_calls((final).function());
 
         Halide::Var _x0, _y1, _c2, _x3, _y4, _c5, _x6, _c8, _x12, _y13, _x15, _y16, _x18, _c20, _x21, _y22, _c23, _y25, _x27, _y28;
-        Halide::Func(funcs["clamped"])
-        .compute_root()
-        ;
-        Halide::Func(funcs["upsampledx$2"])
-        .compute_at(Halide::Func(funcs["upsampled$2"]), _y22)
-        ;
-        Halide::Func(funcs["upsampled$2"])
-        .split(x, x, _x21, 4)
-        .split(y, y, _y22, 8)
-        .reorder(_y22, _x21, y, c, x)
-        .compute_root()
-        ;
+        clamped.compute_root();
+        upsampled
+            .split(x, x, _x21, 4)
+            .split(y, y, _y22, 8)
+            .reorder(_y22, _x21, y, c, x)
+            .compute_root();
+        upsampledx.compute_at(upsampled, _y22);
 
         _autotune_timing_stub(final);
     };
